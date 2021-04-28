0c8b67053bac2144f783532f79a44c46ff24f969
Casey Nord <nordcw@gmail.com>
2021-04-27 22:20:37 -0700

    setting up code file

---
+++ hw04_unit_testing.py
@@ -0,0 +1,35 @@
+# cs362 - hw04
+# casey nord
+# spring 2021
+
+# ---- question 1 ------------------------------------------------------------
+
+# ---- 1.1 TODO: calculate volume of a cube ----
+
+# ---- 1.2 TODO: write 3 unit tests ----
+# negative values
+# type errors
+# complex numbers
+
+# TODO: explain tests in word doc
+
+
+# ---- question 2 ------------------------------------------------------------
+
+# ---- 2.1 TODO: calculate average of elements in a list ----
+
+# ---- 2.2 TODO: write 3 unit tests ----
+# ckecking empty
+# divide by zero
+# ...
+
+# TODO: explain tests in word doc
+
+
+# ---- question 3 ------------------------------------------------------------
+
+# ---- 2.1 TODO: generates full name with provided first and last name ----
+
+# ---- 2.2 TODO: write 3 unit tests ----
+
+# TODO: explain tests in word doc
