738fbedbb7fe850d0533c7e7608a0833d2b4c461
Casey Nord <nordcw@gmail.com>
2021-04-27 22:51:21 -0700

    cube volume function with unit tests

--- hw04_unit_testing.py
+++ hw04_unit_testing.py
@@ -2,16 +2,35 @@
 # casey nord
 # spring 2021
 
+import unittest
+
+
 # ---- question 1 ------------------------------------------------------------
 
-# ---- 1.1 TODO: calculate volume of a cube ----
+# ---- 1.1 calculate volume of a cube ----
 
-# ---- 1.2 TODO: write 3 unit tests ----
-# negative values
-# type errors
-# complex numbers
+def cube_volume(_length):
+    return abs(pow(_length, 3))
 
-# TODO: explain tests in word doc
+# ---- 1.2 write 3 unit tests ----
+
+class CubeVolumeTests(unittest.TestCase):
+
+    def test_correct_values(self):
+        self.assertEqual(cube_volume(1), 1)
+        self.assertEqual(cube_volume(3), 27)
+        self.assertEqual(cube_volume(256), 16777216)
+
+    def test_value_always_positive(self):
+        self.assertTrue(cube_volume(-1), 1)
+        self.assertTrue(cube_volume(-3), 27)
+        self.assertTrue(cube_volume(-256), 1777216)
+
+    def test_type(self):
+        with self.assertRaises(TypeError):
+            cube_volume("42")
+        with self.assertRaises(TypeError):
+            cube_volume(False)
 
 
 # ---- question 2 ------------------------------------------------------------
@@ -33,3 +52,7 @@
 # ---- 2.2 TODO: write 3 unit tests ----
 
 # TODO: explain tests in word doc
+
+
+if __name__ == '__main__':
+    unittest.main()
